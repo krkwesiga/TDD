9eae0465bcbe8c84d5d540cf57887505a9b1cd55
krkwesiga <krkwesiga@gmail.com>
2013-06-04 23:48:56 +0300

    added unit test for an Empty Array

--- src/ArrayManipulator.java
+++ src/ArrayManipulator.java
@@ -3,18 +3,26 @@ public class ArrayManipulator {
 
 	public int largestOfArray(int[] numbers) {
 
-		int largest = numbers[0];
+		int largest = 0;
+		if(numbers.length==0){
 
-		for(int i=0; i<numbers.length;i++){
+			throw new IllegalArgumentException("No Intergers to compare");
 
-			if(numbers[i]>largest){
+		}else{
 
-				largest = numbers[i];
+			largest = numbers[0];
+
+			for(int i=0; i<numbers.length;i++){
+
+				if(numbers[i]>largest){
+
+					largest = numbers[i];
+
+				}
 
 			}
 
 		}
-
 		return largest;
 	}
 
--- test/ArrayManipulatorTest.java
+++ test/ArrayManipulatorTest.java
@@ -9,16 +9,22 @@ public class ArrayManipulatorTest {
 	
 	@Test
 	public void shouldReturn5GivenArrayof5Only(){
-	
 		Assert.assertEquals(5, arrayManipulatorobj.largestOfArray(new int[]{5}));
-	
 	}
 	
 	@Test
 	public void shouldReturn2GivenArrayOf1and2(){
-		
-		Assert.assertEquals(2, arrayManipulatorobj.largestOfArray(new int[]{1,2}));
-		
+		Assert.assertEquals(2, arrayManipulatorobj.largestOfArray(new int[]{1,2}));		
+	}
+	
+	@Test
+	public void shouldReturn2GivenArrayof2and1and1(){
+		Assert.assertEquals(2, arrayManipulatorobj.largestOfArray(new int[]{2,1,1}));		
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldReturnIllegalArgumentGivenEmptyArray(){
+		arrayManipulatorobj.largestOfArray(new int[]{});
 	}
 	
 	
